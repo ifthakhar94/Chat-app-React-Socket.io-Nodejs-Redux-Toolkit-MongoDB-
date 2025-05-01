bf69d4441f733e37c66f7230b9cc9b5993080258
Iftekhar Mahmud <ifthakhar8@gmail.com>
2025-05-01 17:08:07 +0600

    Client init

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-500 ml'>
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
